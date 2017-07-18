9c3b2524b494adfa9308735f4d284d1d111df417
andyreesecups <andyreesecups@yahoo.com>
2017-07-18 13:03:07 -0500

    added button functionality for add 5 and break 5 clocks

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Run jQuery
+$(document).ready(function() {
+    var buzzer = $("#buzzer")[0];
+    var count = parseInt($("#num").html());
+    var breakTime = parseInt($("#breakNum").html());
+    $("#reset").hide();
+
+
+
+    $("#minus5Clock").click(function() {
+        if (count > 5) {
+            count -= 5;
+            $("#num").html(count);
+            console.log(count);
+
+        }
+    });
+
+    $("#add5Clock").click(function() {
+        count += 5;
+        $("#num").html(count);
+        console.log(count);
+    });
+
+    $("#minus5Break").click(function() {
+        if (breakTime > 5) {
+            breakTime -= 5;
+            $("#breakNum").html(breakTime);
+
+        }
+    });
+
+    $("#add5Break").click(function() {
+        breakTime += 5;
+        $("#breakNum").html(breakTime);
+
+    });
+});
